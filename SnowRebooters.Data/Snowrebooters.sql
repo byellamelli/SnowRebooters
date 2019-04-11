0d26f48ca449f6a77114eab5179b8d152280432d
Norman Parker <NParker@avidxchange.com>
2019-04-11 13:10:07 -0600

    add payee table

--- SnowRebooters.Data/Snowrebooters.sql
+++ SnowRebooters.Data/Snowrebooters.sql
@@ -10,3 +10,17 @@ CREATE TABLE dbo.payer (
     state VARCHAR(2),
     zip VARCHAR(20)
 PRIMARY KEY CLUSTERED);
+
+-- create payee table
+CREATE TABLE dbo.payee (
+    payerId int,
+    name VARCHAR(200),
+    email VARCHAR(1000),
+    phone VARCHAR(50),
+    addr1 VARCHAR(200),
+    addr2 VARCHAR(200),
+    city VARCHAR(200),
+    state VARCHAR(2),
+    zip VARCHAR(20)
+PRIMARY KEY CLUSTERED);
+
