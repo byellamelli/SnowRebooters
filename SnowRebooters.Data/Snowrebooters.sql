b9753fd08534d71b6c5e7e8df77ae3960ef2f075
Norman Parker <NParker@avidxchange.com>
2019-04-11 13:02:10 -0600

    add payer table

--- SnowRebooters.Data/Snowrebooters.sql
+++ SnowRebooters.Data/Snowrebooters.sql
@@ -1,1 +1,12 @@
--- create table
+-- create payer table
+CREATE TABLE dbo.payer (
+    payerId int,
+    name VARCHAR(200),
+    email VARCHAR(1000),
+    phone VARCHAR(50),
+    addr1 VARCHAR(200),
+    addr2 VARCHAR(200),
+    city VARCHAR(200),
+    state VARCHAR(2),
+    zip VARCHAR(20)
+PRIMARY KEY CLUSTERED);
