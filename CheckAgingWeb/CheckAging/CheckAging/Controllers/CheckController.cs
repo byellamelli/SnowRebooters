e915a3a11c99522dd2056c98431105a644a00c71
Tommy Lai <whlai@avidxchange.com>
2019-04-11 15:07:24 -0600

    UI: Added calling stored procedure logic

--- CheckAgingWeb/CheckAging/CheckAging/Controllers/CheckController.cs
+++ CheckAgingWeb/CheckAging/CheckAging/Controllers/CheckController.cs
@@ -1,15 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using CheckAging.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace CheckAging.Controllers
 {
     [Route("api/[controller]")]
     public class CheckController : Controller
     {
+        private readonly IConfiguration configuration;
+
+        public CheckController(IConfiguration config)
+        {
+            configuration = config;
+        }
         [HttpGet("[action]")]
         public IEnumerable<Check> GetChecks()
         {
@@ -17,18 +26,55 @@ namespace CheckAging.Controllers
 
             // hard code
             List<Check> lCheck= new List<Check>();
-            lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com"});
-            lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
-            lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
-            lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
-            lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
-            lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
-            lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
-            lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
-            lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
-            lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
-            lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
+            //lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com"});
+            //lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
+            //lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
+            //lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
+            //lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
+            //lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
+            //lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
+            //lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
+            //lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
+            //lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
+            //lCheck.Add(new Check() { Id = 1, DateIssued = "01/02/2005", DateCleared = "01/02/2005", Amount = "$120", PhoneNumber = "801-998-9876", EmailAddress = "test1@gmail.com" });
 
+            var connection = new SqlConnection("Server=snowrebooters.database.windows.net;Database=snowrebooter;User Id=snowrebooter;Password=Khong!8526Best;TrustServerCertificate=False;");
+
+            var command = new SqlCommand("GetCheckData", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            // command.Parameters.AddWithValue("@WeatherID", 1);
+
+            connection.Open();
+
+            //List<Check> listWeather = new List<Check>();
+
+            using (SqlDataReader rdr = command.ExecuteReader())
+            {
+                // iterate through results, printing each to console
+                while (rdr.Read())
+                {
+                    Check c = new Check();
+
+                    c.Id = Convert.ToInt32(rdr["Id"]);
+                    c.DateIssued = rdr["dateIssued"].ToString();
+                    c.DateCleared = rdr["datetime"].ToString();
+                    c.Amount = rdr["amount"].ToString();
+                    // c.Payee = rdr["Id"].ToString();
+                    c.PhoneNumber = rdr["Phone "].ToString();
+                    c.EmailAddress = rdr["email"].ToString();
+
+                    lCheck.Add(c);
+                    // Console.WriteLine("Product: {0,-35} Total: {1,2}", rdr["ProductName"], rdr["Total"]);
+
+                }
+
+
+
+                // command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+            
             return lCheck;
         }
 
--- CheckAgingWeb/CheckAging/CheckAging/Startup.cs
+++ CheckAgingWeb/CheckAging/CheckAging/Startup.cs
@@ -28,6 +28,8 @@ namespace CheckAging
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddSingleton<IConfiguration>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
